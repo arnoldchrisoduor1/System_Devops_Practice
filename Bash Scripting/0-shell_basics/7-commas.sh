ed4ac7095522dc275a43933cab02dcb81eea8d67
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-03-08 12:58:06 +0300

    listing and sorting files

---
+++ Bash Scripting/0-shell_basics/7-commas.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# List all files and folders including the hidden.
+# Add trailing slash to the directories.
+# Sort the results alphanumerically, dont have to be case sensitive.
+
+ls -map | sort -d
