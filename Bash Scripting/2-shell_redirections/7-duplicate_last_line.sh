6059f7b246d1f89134e1298c9993611b4e19fcc4
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-03-12 15:29:20 +0300

    deleting all the javascript files

---
+++ Bash Scripting/2-shell_redirections/7-duplicate_last_line.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Gets the last line of the file iacta and appends it to the same file, essentially duplicating the last line.
+
+tail -n 1 iacta >> iacta
---
+++ Bash Scripting/2-shell_redirections/8-no_more_js.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Finding all the files ending with the .js extension and deleting them in the current duectory.
+
+find . -type f -name "*.js" -delete
