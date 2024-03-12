40ec30360fecca0e8f74315eb428a195ed0e0b78
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-03-12 15:11:48 +0300

    getting the third line of the file iacta

---
+++ Bash Scripting/2-shell_redirections/5-third_line.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# First getting the first 3 lines of the file iacta
+
+# Secong is getting the last line of the selected three lines, basically the third line of the file iacta.
+
+head -n 3 iacta | tail -n 1
